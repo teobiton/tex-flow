591e3b61a78dac754957f460e4dee2e55970276a
Louison_GOUY <louison.gouy@etu.univ-nantes.fr>
2022-12-10 23:20:15 +0100

    Create script for git init

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Script for set up
-
-target_path="."
-
-# Format Tex files automatically before commit
-#   replace defaulf file <.git/pre_commit>
-# TODO add 'if git repo in target_path'
-cat $target_path/pre-commit.txt > .git/pre_commit
